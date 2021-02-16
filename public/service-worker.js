c39320d774e396533bc45c83b9eb25a36a694f44
Emily Thomas <ethomas@quint.co.uk>
2021-02-15 19:45:41 -0800

    created service worker

---
+++ public/service-worker.js
@@ -0,0 +1,70 @@
+const FILES_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/index.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+const NAME = "name";
+const DATA_CACHE = "data-cache";
+
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(NAME).then((cache) => {
+      console.log("Success");
+      return cache.addAll(FILES_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== NAME && key !== DATA_CACHE) {
+            console.log("Updated", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
+    })
+  );
+});
